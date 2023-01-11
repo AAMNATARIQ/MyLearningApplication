60b353a56b57f24457e6247f4cbcf6271bf1edf3
Amna <aamnaht4@gmail.com>
2023-01-11 20:23:03 +0500

    basic functionality of learnig application with last assignment done

---
+++ app/src/main/java/amnatariq/org/mylearningapplication/StartApp.java
@@ -0,0 +1,17 @@
+package amnatariq.org.mylearningapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class StartApp extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_app);
+    }
+}
